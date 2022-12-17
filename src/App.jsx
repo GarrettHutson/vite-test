88d0c2795eafbbc38f2c8237a3a67631fe22cc1c
garrett hutson <hutsongarrett@gmail.com>
2022-12-17 10:06:05 -0500

    checking to see if netlify will update my site

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ export default function App() {
     return (
       <>
         <div className="bg-black w-screen h-screen text-white">Hello World</div>
+        <div>This is a test</div>
       </>
     );
   }
